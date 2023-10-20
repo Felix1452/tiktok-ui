c7f8bbcd8f62ce590fa150d92cde474661210ff8
Phuc <115924067+Phuchuynh1452@users.noreply.github.com>
2023-10-20 15:59:45 +0700

    initinstall

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './view/Login';
+import PageNotFound from './view/PageNotFound';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" Component={Login} />
+      <Route path="*" Component={PageNotFound} />
+    </Routes>
+  </Router>
+
+  // {/* </React.StrictMode> */}
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/view/PageNotFound.js
@@ -0,0 +1,6 @@
+function PageNotFound () {
+    return (
+        <h3>Page Not Found</h3>
+    )
+};
+export default PageNotFound;
